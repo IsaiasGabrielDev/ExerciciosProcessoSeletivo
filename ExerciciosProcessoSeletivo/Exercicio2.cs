418b26ef7a58f0160618f75f5ad98a32b340aecf
IsaiasGabriel <isaiasgabrielhub@gmail.com>
2021-05-21 18:07:11 -0300

    primeiro commit

---
+++ ExerciciosProcessoSeletivo/Exercicio2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProcessoSeletivo
+{
+    public class Exercicio2
+    {
+        /// <summary>
+        /// ORDENE EM ORDEM CRESCENTE E RETORNE A VARIAVEL CODIGOS
+        /// </summary>
+        /// <param name="codigos"></param>
+        /// <returns></returns>
+        
+        
+        internal List<int> GetCodigosOrdenados(List<int> codigos)
+        {
+            var codigos_1 = new List<int>();
+          
+            var cods = codigos.OrderBy(c => c);
+            
+            foreach (var c in cods)
+	        {
+                codigos_1.Add(c);
+	        }
+            return codigos_1;
+        }
+       
+
+
+    }
+}
